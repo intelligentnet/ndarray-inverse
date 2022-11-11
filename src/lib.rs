6d2aadd3f71d423b765d484ce526cc86da06754e
Chris Dipple <chris@intelligent-net.co.uk>
2022-11-11 11:32:52 +0000

    Speed tweeks and better testing

--- src/lib.rs
+++ src/lib.rs
@@ -17,11 +17,15 @@ where
     T: Float + Debug + ScalarOperand + Sum<T>,
 {
     fn det(&self) -> T {
-        determinant(self)
+        let s = self.raw_dim();
+        assert!(s[0] == s[1]);
+        // Flatten to Vec!
+        let vm: Vec<T> = self.iter().map(|&i| i).collect();
+        determinant(&vm, s[0])
     }
 
     fn inv(&self) -> Option<Self> {
-        // Seems fasster if not inlined!
+        // Seems faster if not inlined!
         fn submat<'a, T: Float>(res: &'a mut Vec<T>, m: &'a Array2<T>, sr: usize, sc: usize) {
             let s = m.raw_dim();
             let mut i: usize = 0;
@@ -31,14 +35,13 @@ where
                     i += 1;
                 }
             }));
-            //(i..l*l).for_each(|i| res[i] = T::zero()); // must fill rest with 0
             // Note: to stop copy we do not return anything. Variable mutated!
         }
 
         let s = self.raw_dim();
         assert!(s[0] == s[1]);
         let l = s[0];
-        let det = determinant(self);
+        let det = self.det();
         if !det.is_zero() {
             match l {
                 1 => Some(array![[self[(0, 0)].recip()]]),
@@ -218,8 +221,7 @@ where
                         for i in 0..l {
                             for j in 0..l {
                                 submat(&mut res, self, i, j);
-                                let sm: Array2<T> = unsafe { Array2::from_shape_vec_unchecked((l - 1, l - 1), res.to_vec()) };
-                                let d = determinant(&sm);
+                                let d = determinant(&res, l - 1);
                                 cofactors[(j, i)] = if ((i + j) % 2) == 0 { d } else { -d };
                             }
                         }
@@ -235,48 +237,48 @@ where
     }
 }
 
-fn determinant<T>(m: &Array2<T>) -> T
+fn determinant<T>(vm: &Vec<T>, l: usize) -> T
 where
     T: Copy + Zero + One + Mul + Sub<Output=T> + Neg<Output=T> + Sum<T>,
 {
-    let l = m.raw_dim()[0];
+    // Must be square matrix l x l!
     // Fully expanding the first few determinants makes a big
     // difference, especially as the generic algorithm is recursive
     match l {
-        1 => m[(0, 0)],
-        2 => m[(0, 0)] * m[(1, 1)] - m[(1, 0)] * m[(0, 1)],
+        1 => vm[0],
+        2 => vm[0] * vm[3] - vm[2] * vm[1],
         3 => {
-            let m00 = m[(0, 0)];
-            let m01 = m[(0, 1)];
-            let m02 = m[(0, 2)];
-            let m10 = m[(1, 0)];
-            let m11 = m[(1, 1)];
-            let m12 = m[(1, 2)];
-            let m20 = m[(2, 0)];
-            let m21 = m[(2, 1)];
-            let m22 = m[(2, 2)];
+            let m00 = vm[0];
+            let m01 = vm[1];
+            let m02 = vm[2];
+            let m10 = vm[3];
+            let m11 = vm[4];
+            let m12 = vm[5];
+            let m20 = vm[6];
+            let m21 = vm[7];
+            let m22 = vm[8];
 
             m00 * (m11 * m22 - m12 * m21) - m01
                 * (m10 * m22 - m12 * m20) + m02
                 * (m10 * m21 - m11 * m20)
         }
         4 => {
-            let m00 = m[(0, 0)];
-            let m01 = m[(0, 1)];
-            let m02 = m[(0, 2)];
-            let m03 = m[(0, 3)];
-            let m10 = m[(1, 0)];
-            let m11 = m[(1, 1)];
-            let m12 = m[(1, 2)];
-            let m13 = m[(1, 3)];
-            let m20 = m[(2, 0)];
-            let m21 = m[(2, 1)];
-            let m22 = m[(2, 2)];
-            let m23 = m[(2, 3)];
-            let m30 = m[(3, 0)];
-            let m31 = m[(3, 1)];
-            let m32 = m[(3, 2)];
-            let m33 = m[(3, 3)];
+            let m00 = vm[0];
+            let m01 = vm[1];
+            let m02 = vm[2];
+            let m03 = vm[3];
+            let m10 = vm[4];
+            let m11 = vm[5];
+            let m12 = vm[6];
+            let m13 = vm[7];
+            let m20 = vm[8];
+            let m21 = vm[9];
+            let m22 = vm[10];
+            let m23 = vm[11];
+            let m30 = vm[12];
+            let m31 = vm[13];
+            let m32 = vm[14];
+            let m33 = vm[15];
 
             // Note: Compiler optimisation will remove redundant multiplies
             m00 * m21 * m32 * m13 -
@@ -305,31 +307,31 @@ where
                 m33 * m02 * m11 * m20
         }
         5 => {
-            let m00 = m[(0, 0)];
-            let m01 = m[(0, 1)];
-            let m02 = m[(0, 2)];
-            let m03 = m[(0, 3)];
-            let m04 = m[(0, 4)];
-            let m10 = m[(1, 0)];
-            let m11 = m[(1, 1)];
-            let m12 = m[(1, 2)];
-            let m13 = m[(1, 3)];
-            let m14 = m[(1, 4)];
-            let m20 = m[(2, 0)];
-            let m21 = m[(2, 1)];
-            let m22 = m[(2, 2)];
-            let m23 = m[(2, 3)];
-            let m24 = m[(2, 4)];
-            let m30 = m[(3, 0)];
-            let m31 = m[(3, 1)];
-            let m32 = m[(3, 2)];
-            let m33 = m[(3, 3)];
-            let m34 = m[(3, 4)];
-            let m40 = m[(4, 0)];
-            let m41 = m[(4, 1)];
-            let m42 = m[(4, 2)];
-            let m43 = m[(4, 3)];
-            let m44 = m[(4, 4)];
+            let m00 = vm[0];
+            let m01 = vm[1];
+            let m02 = vm[2];
+            let m03 = vm[3];
+            let m04 = vm[4];
+            let m10 = vm[5];
+            let m11 = vm[6];
+            let m12 = vm[7];
+            let m13 = vm[8];
+            let m14 = vm[9];
+            let m20 = vm[10];
+            let m21 = vm[11];
+            let m22 = vm[12];
+            let m23 = vm[13];
+            let m24 = vm[14];
+            let m30 = vm[15];
+            let m31 = vm[16];
+            let m32 = vm[17];
+            let m33 = vm[18];
+            let m34 = vm[19];
+            let m40 = vm[20];
+            let m41 = vm[21];
+            let m42 = vm[22];
+            let m43 = vm[23];
+            let m44 = vm[24];
 
             // Note: Compiler optimisation will remove redundant multiplies
             m00 * m11 * m22 * m33 * m44
@@ -455,42 +457,42 @@ where
         }
         6 => {
             // Okay, getting very unweldy but worth it
-            let m00 = m[(0, 0)];
-            let m01 = m[(0, 1)];
-            let m02 = m[(0, 2)];
-            let m03 = m[(0, 3)];
-            let m04 = m[(0, 4)];
-            let m05 = m[(0, 5)];
-            let m10 = m[(1, 0)];
-            let m11 = m[(1, 1)];
-            let m12 = m[(1, 2)];
-            let m13 = m[(1, 3)];
-            let m14 = m[(1, 4)];
-            let m15 = m[(1, 5)];
-            let m20 = m[(2, 0)];
-            let m21 = m[(2, 1)];
-            let m22 = m[(2, 2)];
-            let m23 = m[(2, 3)];
-            let m24 = m[(2, 4)];
-            let m25 = m[(2, 5)];
-            let m30 = m[(3, 0)];
-            let m31 = m[(3, 1)];
-            let m32 = m[(3, 2)];
-            let m33 = m[(3, 3)];
-            let m34 = m[(3, 4)];
-            let m35 = m[(3, 5)];
-            let m40 = m[(4, 0)];
-            let m41 = m[(4, 1)];
-            let m42 = m[(4, 2)];
-            let m43 = m[(4, 3)];
-            let m44 = m[(4, 4)];
-            let m45 = m[(4, 5)];
-            let m50 = m[(5, 0)];
-            let m51 = m[(5, 1)];
-            let m52 = m[(5, 2)];
-            let m53 = m[(5, 3)];
-            let m54 = m[(5, 4)];
-            let m55 = m[(5, 5)];
+            let m00 = vm[0];
+            let m01 = vm[1];
+            let m02 = vm[2];
+            let m03 = vm[3];
+            let m04 = vm[4];
+            let m05 = vm[5];
+            let m10 = vm[6];
+            let m11 = vm[7];
+            let m12 = vm[8];
+            let m13 = vm[9];
+            let m14 = vm[10];
+            let m15 = vm[11];
+            let m20 = vm[12];
+            let m21 = vm[13];
+            let m22 = vm[14];
+            let m23 = vm[15];
+            let m24 = vm[16];
+            let m25 = vm[17];
+            let m30 = vm[18];
+            let m31 = vm[19];
+            let m32 = vm[20];
+            let m33 = vm[21];
+            let m34 = vm[22];
+            let m35 = vm[23];
+            let m40 = vm[24];
+            let m41 = vm[25];
+            let m42 = vm[26];
+            let m43 = vm[27];
+            let m44 = vm[28];
+            let m45 = vm[29];
+            let m50 = vm[30];
+            let m51 = vm[31];
+            let m52 = vm[32];
+            let m53 = vm[33];
+            let m54 = vm[34];
+            let m55 = vm[35];
 
             // Note: Compiler optimisation will remove redundant multiplies
             m00 * m11 * m22 * m33 * m44 * m55
@@ -1218,20 +1220,21 @@ where
         // Now do it the traditional way
         {
             // Reuseable result to reduce memory allocations
-            let mut res = Array2::<T>::zeros((l - 1, l - 1));
-            (0..l)
+            let l1 = l - 1;
+            let mut res: Vec<T> = vec![T::zero(); l1 * l1];
+            (0 .. l)
                 .map(|i| {
-                    for r in 0..l - 1 {
-                        for c in 0..l - 1 {
-                            res[(r, c)] = 
+                    for r in 0 .. l1 {
+                        for c in 0 .. l1 {
+                            res[r * l1 + c] = 
                                 if c < i {
-                                    m[(r + 1, c)]
+                                    vm[(r + 1) * l + c]
                                 } else {
-                                    m[(r + 1, c + 1)]
+                                    vm[(r + 1) * l  + (c + 1)]
                                 };
                         }
                     }
-                    let v = m[(0, i)] * determinant(&res);
+                    let v = vm[i] * determinant(&res, l1);
                     if (i % 2) == 0 {
                         v
                     } else {
@@ -1342,6 +1345,29 @@ mod test_inverse {
     }
 
     #[test]
+    fn test_5x5b() {
+        let a: Array2<f64> = array![
+              [8.12658, 1.02214, 2.67754, 7.00746, 0.16869],
+              [3.31581, 0.63792, 4.28436, 5.61229, 1.19785],
+              [8.13028, 6.48437, 6.91838, 2.68860, 6.60971],
+              [2.85578, 4.40362, 1.13265, 8.32311, 4.41366],
+              [7.93379, 4.09682, 4.86444, 6.67282, 3.57098]
+        ];
+        let inv = a.inv().expect("Sods Law");
+        let back = inv.inv().unwrap();
+        // Precision is good here!
+        assert!(compare_vecs(&a, &back, EPS));
+        let expected: Array2<f64> = array![
+            [ 0.50074759, -0.02510542,  0.33877844,  0.09275141, -0.7569348 ],
+            [-1.58848174, -0.64356647, -1.32943575, -0.57849057,  3.46664012],
+            [-0.49917931,  0.12378294, -0.30452856, -0.25789124,  0.86447499],
+            [-0.0513424 ,  0.06348965, -0.1164256 ,  0.0585164 ,  0.12430141],
+            [ 1.48578933,  0.50685455,  1.40491123,  0.69956389, -3.42524083]
+        ];
+        assert!(compare_vecs(&inv, &expected, EPS));
+    }
+
+    #[test]
     fn test_6x6() {
         let a: Array2<f64> = array![
             [1.0, 1.0, 3.0, 4.0, 9.0, 3.0],
@@ -1368,6 +1394,36 @@ mod test_inverse {
             [-3.90709046,  2.46699267,  4.17114914, -0.87041565, -0.31051345,
              -6.07579462]
         ];
+        assert!(compare_vecs(&inv, &expected, EPS));
+    }
+
+    #[test]
+    fn test_7x7() {
+        let a:Array2<f64> = array![
+            [4.3552 , 6.25851, 4.12662, 1.93708, 0.21272, 3.25683, 6.53326],
+            [4.24746, 1.84137, 6.71904, 0.59754, 3.5806 , 3.63597, 5.347  ],
+            [2.30479, 1.70591, 3.05354, 1.82188, 5.27839, 7.9166 , 2.04607],
+            [2.40158, 6.38524, 7.90296, 4.69683, 6.63801, 7.32958, 1.45936],
+            [0.42456, 6.47456, 1.55398, 8.28979, 4.20987, 0.90401, 4.94587],
+            [5.78903, 1.92032, 6.20261, 5.78543, 1.94331, 8.25178, 7.47273],
+            [1.44797, 7.41157, 7.69495, 8.90113, 3.05983, 0.41582, 6.42932]];
+        let inv = a.inv().expect("Sods Law");
+        let back = inv.inv();
+        assert!(compare_vecs(&a, &back.unwrap(), EPS));
+        let expected = array![[-0.0819729 ,  0.30947774, -0.90286793,  0.49545542,
+                   0.47316173, 0.30651669, -0.71946285],
+                   [ 0.15235561, -0.07559205, -0.0070691 ,  0.06757486,
+                   0.01026404, -0.08019532, -0.01972633],
+                   [-0.03064041, -0.00625996,  0.07810921, -0.01780696,
+                   -0.19716224, -0.03302802,  0.20558502],
+                   [-0.11358415, -0.02543206, -0.24035635,  0.1260263 ,
+                   0.1346972 , 0.16555344, -0.111583  ],
+                   [-0.1016236 ,  0.21480081, -0.0698384 ,  0.05521421,
+                   0.20158013, -0.05049775, -0.16205801],
+                   [ 0.0760218 , -0.20124886,  0.34308958, -0.12448475,
+                   -0.18988991, -0.02734616,  0.18705112],
+                   [ 0.08020185, -0.02906765,  0.46181332, -0.36087425,
+                   -0.15255756, -0.14045526,  0.31376606]];
         assert!(compare_vecs(&inv, &expected, EPS));
     }
 
